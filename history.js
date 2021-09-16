aa39d35176b81de2f9cc9f02536f33330824953e
kirstenlindsmith <kirstenlindsmith@gmail.com>
2021-09-16 13:41:20 -0400

    Updates

--- history.js
+++
@@ -1,7 +0,0 @@
-const createBrowserHistory = require('history').createBrowserHistory
-const createMemoryHistory = require('history').createMemoryHistory
-
-const history =
-  process.env.NODE_ENV === 'test' ? createMemoryHistory() : createBrowserHistory()
-
-export default history
